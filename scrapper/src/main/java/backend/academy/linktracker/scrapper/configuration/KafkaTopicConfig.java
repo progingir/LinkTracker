06500db6ac0091f6b4750b2f73c6579489395106
Valerii_Butko <lb2005lba@gmail.com>
2026-04-28 10:30:50 +0500

    hw-5: фикс тестов

--- scrapper/src/main/java/backend/academy/linktracker/scrapper/configuration/KafkaTopicConfig.java
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/configuration/KafkaTopicConfig.java
@@ -10,11 +10,16 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
     @Bean
-    public NewTopic linkUpdatesTopic(@Value("${app.kafka.topic}") String topicName) {
+    public NewTopic linkUpdatesTopic(
+            @Value("${app.kafka.topic}") String topicName,
+            @Value("${app.kafka.partitions:3}") int partitions,
+            @Value("${app.kafka.replicas:3}") int replicas,
+            @Value("${app.kafka.min-insync-replicas:2}") String minInsyncReplicas) {
+
         return TopicBuilder.name(topicName)
-                .partitions(3)
-                .replicas(3)
-                .configs(Map.of("min.insync.replicas", "2"))
+                .partitions(partitions)
+                .replicas(replicas)
+                .configs(Map.of("min.insync.replicas", minInsyncReplicas))
                 .build();
     }
 }
--- scrapper/src/test/java/backend/academy/linktracker/scrapper/ScrapperIntegrationTestBase.java
+++ scrapper/src/test/java/backend/academy/linktracker/scrapper/ScrapperIntegrationTestBase.java
@@ -14,8 +14,10 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
